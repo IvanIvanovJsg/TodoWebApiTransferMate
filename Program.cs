561ab353818c209b8eccd41c20770436a1fc8f86
Ivan Ivanov <ivanivanov.jsg@gmail.com>
2024-11-12 15:25:06 +0200

    Fixed a problem the db connection

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using TodoWebApiTransferMate.Data;
+using TodoWebApiTransferMate.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // TODO: Add logging middleware
 // TODO: Make swagger be on the main endpoint
 // TODO: See what to do with the connection string and the credentials in docker-compose
+// TODO: Change host name depending on who runs dotnet ef dattabase update
+
 builder.Services.AddDbContext<TodoDbContext>(options =>
-    options.UseNpgsql("Host=localhost;Port=5432;Database=TodoDb;Username=transfermate;Password=transfermatepassword"));
+    options.UseNpgsql("Host=db;Port=5432;Database=TodoDb;Username=transfermate;Password=transfermatepassword"));
+builder.Services.AddScoped<ITodoTaskService, TodoTaskService>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
